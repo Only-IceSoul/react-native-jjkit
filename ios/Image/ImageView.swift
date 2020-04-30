7690490744c476ae1df48e87678da134d3aee791
Juan J LF <justin@.local>
2020-04-29 22:01:25 -0400

    img ch

--- ios/Image/ImageView.swift
+++ ios/Image/ImageView.swift
@@ -17,8 +17,6 @@ class ImageView: UIImageView , ViewTarget {
         clipsToBounds = true
     }
     
-    private var mTagText = ""
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         contentMode = .scaleAspectFill
@@ -29,6 +27,7 @@ class ImageView: UIImageView , ViewTarget {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     @objc func setScaleType(_ scaleType:Int){
         switch scaleType {
         case 1:
@@ -38,97 +37,79 @@ class ImageView: UIImageView , ViewTarget {
         }
     }
     
-
     @objc func setData(_ data:[String:Any]?){
+
         if data != nil {
             let w = data!["width"] as? CGFloat ?? -1
             let h = data!["height"] as? CGFloat ?? -1
             let cache = data!["cache"] as? Bool ?? false
+            let asGif = data!["asGif"] as? Bool ?? false
             
-            if let url = data!["url"] as? String , let type = data!["type"] as? String{
-                
+            if let url = data!["url"] as? String {
                 let reqW = w > 20 ? w : 20
                 let reqH = h > 20 ? w : 20
-                updateImage(url, cache, type, w, h, reqW, reqH)
+                
+                updateImage(url, cache, asGif, w, h, reqW, reqH)
+               
             }
         }
     }
     
-    private func updateImage(_ url:String,_ cache:Bool,_ type:String,_ w:CGFloat,_ h:CGFloat,_ reqW:CGFloat,_ reqH:CGFloat){
+    private func updateImage(_ url:String,_ cache:Bool,_ asGif:Bool,_ w:CGFloat,_ h:CGFloat,_ reqW:CGFloat,_ reqH:CGFloat){
             if (cache) {
                 if (w != 1 && h != -1) {
-                    updateImageCache(url,type,reqW,reqH)
+                    updateImageCache(url,asGif,reqW,reqH)
                 }else {
-                    updateImageCache(url,type)
+                    updateImageCache(url,asGif)
                 }
             }else {
                 if (w != 1 && h != -1)  {
-                    updateImageNoCache(url,type,reqW,reqH)
+                    updateImageNoCache(url,asGif,reqW,reqH)
                 }else {
-                    updateImageNoCache(url,type)
+                    updateImageNoCache(url,asGif)
                 }
             }
     }
     
-      private func updateImageCache(_ url:String,_ type:String,_ w:CGFloat,_ h:CGFloat){
-            if(type == "image"){
-         
-                Guiso.load(url).fitCenter().override(w,h).into(self)
-            }
-            if(type == "video"){
-                Guiso.load(url).frame(1)
-                    .fitCenter().override(w,h).into(self)
-            }
-            if (type == "gif"){
-                Guiso.load(url).asGif().fitCenter().override(w,h).into(self)
-            }
-
+    private func updateImageCache(_ url:String,_ asGif:Bool,_ w:CGFloat,_ h:CGFloat){
+        if (asGif){
+            Guiso.load(url).asGif().fitCenter().override(w,h).into(self)
+        }else{
+           Guiso.load(url).frame(1)
+               .fitCenter().override(w,h).into(self)
         }
+    }
 
-        private func updateImageCache(_ url:String,_ type:String){
-            if(type == "image"){
-                Guiso.load(url).into(self)
-            }
-            if(type == "video"){
-                Guiso.load(url).frame(1).into(self)
-            }
-            if (type == "gif"){
-                Guiso.load(url).asGif().into(self)
-           }
+    private func updateImageCache(_ url:String,_ asGif:Bool){
+        if (asGif){
+            Guiso.load(url).asGif().into(self)
+        }else{
+            Guiso.load(url).frame(1).into(self)
         }
+    }
 
-        private func updateImageNoCache(_ url:String,_ type:String,_ w:CGFloat,_ h:CGFloat){
-            if(type == "image"){
-              Guiso.load(url)
-                .skipMemoryCache()
-                .fitCenter().override(w,h).into(self)
-            }
-            if(type == "video"){
-                 Guiso.load(url).frame(1)
-                    .skipMemoryCache()
-                    .fitCenter().override(w,h).into(self)
-            }
-            if type == "gif" {
-                Guiso.load(url).asGif()
-                .skipMemoryCache()
-                .fitCenter().override(w,h).into(self)
-            }
+    private func updateImageNoCache(_ url:String,_ asGif:Bool,_ w:CGFloat,_ h:CGFloat){
+        if asGif {
+            Guiso.load(url).asGif()
+            .skipMemoryCache()
+            .fitCenter().override(w,h).into(self)
+        }else{
+            Guiso.load(url).frame(1)
+            .skipMemoryCache()
+            .fitCenter().override(w,h).into(self)
         }
-        private func updateImageNoCache(_ url:String,_ type:String){
-            if(type == "image"){
-            Guiso.load(url)
-              .skipMemoryCache().into(self)
-            }
-            if(type == "video"){
-               Guiso.load(url).frame(1)
-                  .skipMemoryCache()
-                  .into(self)
-            }
-            if type == "gif" {
-               Guiso.load(url).asGif()
-               .skipMemoryCache()
-               .into(self)
-            }
+    }
+    
+    private func updateImageNoCache(_ url:String,_ asGif:Bool){
+        if asGif {
+           Guiso.load(url).asGif()
+           .skipMemoryCache()
+           .into(self)
+        }else{
+            Guiso.load(url).frame(1)
+                        .skipMemoryCache()
+                        .into(self)
+        }
     }
 
     
