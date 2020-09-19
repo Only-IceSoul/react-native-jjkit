2085ec0a0da150a87fdc1ddee39ed45f37be4333
Juan <justinjlf21@gmail.com>
2020-09-19 11:55:21 -0400

    image ph and listeners -a

--- android/src/main/java/com/reactjjkit/image/ImageViewManager.kt
+++ android/src/main/java/com/reactjjkit/image/ImageViewManager.kt
@@ -2,12 +2,21 @@ package com.reactjjkit.image
 
 import android.content.Context
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.common.MapBuilder
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
 
 class ImageViewManager : SimpleViewManager<JJImageView>() {
+
+    companion object{
+        const val EVENT_ON_LOAD_START = "onLoadStart"
+        const val EVENT_ON_LOAD_END = "onLoadEnd"
+        const val EVENT_ON_LOAD_FAILED = "onLoadFailed"
+        const val EVENT_ON_LOAD_SUCCESS = "onLoadSuccess"
+    }
 
     override fun getName(): String {
         return "Image"
@@ -17,16 +26,31 @@ class ImageViewManager : SimpleViewManager<JJImageView>() {
         return  JJImageView(reactContext as Context)
     }
 
-    @ReactProp(name = "data")
-    fun data(view: JJImageView, data: ReadableMap?)  {
-        view.setData(data)
+    @ReactProp(name = "source")
+    fun source(view: JJImageView, data: ReadableMap?)  {
+        view.setSrc(data)
     }
 
-    @ReactProp(name = "scaleType")
+    @ReactProp(name = "scaleType",defaultInt = 1)
     fun scaleType(view: JJImageView, scaleType: Int) {
         when(scaleType){
             1 -> view.scaleType = ImageView.ScaleType.FIT_CENTER
             else -> view.scaleType = ImageView.ScaleType.CENTER_CROP
         }
+    }
+    override fun onDropViewInstance(view: JJImageView) {
+
+        Glide.with(view.context).clear(view)
+
+        super.onDropViewInstance(view)
+    }
+
+    override fun getExportedCustomBubblingEventTypeConstants(): MutableMap<String, Any> {
+        return MapBuilder.builder<String,Any>()
+                .put(EVENT_ON_LOAD_START, MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", EVENT_ON_LOAD_START)))
+                .put(EVENT_ON_LOAD_END, MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", EVENT_ON_LOAD_END)))
+                .put(EVENT_ON_LOAD_FAILED, MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", EVENT_ON_LOAD_FAILED)))
+                .put(EVENT_ON_LOAD_SUCCESS, MapBuilder.of("phasedRegistrationNames", MapBuilder.of("bubbled", EVENT_ON_LOAD_SUCCESS)))
+                .build()
     }
 }
--- android/src/main/java/com/reactjjkit/image/JJImageView.kt
+++ android/src/main/java/com/reactjjkit/image/JJImageView.kt
@@ -1,19 +1,45 @@
 package com.reactjjkit.image
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.Outline
+import android.os.Handler
+import android.os.Looper
+import android.util.Base64
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.drawable.toDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.uimanager.events.RCTEventEmitter
+import java.lang.Exception
+import java.lang.ref.WeakReference
+import java.net.URL
 import android.view.ViewOutlineProvider as ViewOutlineProvider1
 
 class JJImageView(context: Context) : AppCompatImageView(context) {
 
+
+    companion object{
+        const val EVENT_ON_LOAD_START = "onLoadStart"
+        const val EVENT_ON_LOAD_END = "onLoadEnd"
+        const val EVENT_ON_LOAD_FAILED = "onLoadFailed"
+        const val EVENT_ON_LOAD_SUCCESS = "onLoadSuccess"
+    }
+
     init {
-        scaleType = ScaleType.CENTER_CROP
         clipToOutline = true
         outlineProvider = object: ViewOutlineProvider1(){
             override fun getOutline(view: View?, outline: Outline?) {
@@ -23,164 +49,148 @@ class JJImageView(context: Context) : AppCompatImageView(context) {
         }
     }
 
-    fun setData(data: ReadableMap?){
+    fun setSrc(data: ReadableMap?){
         if(data != null) {
             val w = data.getInt("width")
             val h = data.getInt("height")
             val cache = data.getBoolean("cache")
             val url = data.getString("url")
             val asGif = data.getBoolean("asGif")
+            val placeholder = data.getString("placeholder")
+
             if (url != null ) {
+                val resize = w != -1 && h != -1
                 val reqW = if (w > 20) w else 20
-                val reqH = if (h > 20) w else 20
-                updateImage(url, cache, asGif, w, h, reqW, reqH)
+                val reqH = if (h > 20) h else 20
+                updateImage(url,placeholder, cache, asGif,resize,reqW, reqH)
             }
         }
     }
 
-    private fun updateImage(url:String,cache:Boolean,asGif:Boolean,w:Int,h:Int,reqW:Int,reqH:Int){
-        if (cache) {
-            if (w != 1 && h != -1) {
-                updateImageCache(url,asGif,reqW,reqH)
-            }else {
-                updateImageCache(url,asGif)
-            }
-        }else {
-            if (w != 1 && h != -1)  {
-                updateImageNoCache(url,asGif,reqW,reqH)
-            }else {
-                updateImageNoCache(url,asGif)
-            }
-        }
-    }
-    private fun updateImageCache(url:String,asGif:Boolean,w:Int,h:Int){
-        if (asGif ){
-            val options =  RequestOptions()
-                    .fitCenter()
-                    .override(w, h)
+    private fun updateImage(url:String,placeholder:String?,cache:Boolean,asGif:Boolean,resize:Boolean,reqW:Int,reqH:Int){
+        val reactContext = WeakReference(context as ReactContext)
+        Thread{
+            val options = getOptions(asGif,cache,placeholder,resize,reqW,reqH)
 
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context).asGif().load(bytes).apply(options).into(this)
-            }else{
-                Glide.with(context).asGif().load(url).apply(options).into(this)
-            }
+            Handler(Looper.getMainLooper()).post{
 
-        }else{
-            val options =  RequestOptions()
-                    .frame(0L)
-                    .fitCenter()
-                    .override(w, h)
-            if ( url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context)
-                        .asBitmap()
-                        .load(bytes)
-                        .apply(options)
+                var manager  = if(asGif){
+                    Glide.with(context).asGif()
+                            .listener(object: RequestListener<GifDrawable> {
+                                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<GifDrawable>?, isFirstResource: Boolean): Boolean {
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_FAILED, Arguments.createMap())
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_END, Arguments.createMap())
+                                    return false
+                                }
+
+                                override fun onResourceReady(resource: GifDrawable?, model: Any?, target: Target<GifDrawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_SUCCESS, Arguments.createMap())
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_END, Arguments.createMap())
+                                    return false
+                                }
+                            })
+                } else {
+                    Glide.with(context)
+                            .asBitmap()
+                            .listener(object: RequestListener<Bitmap> {
+                                override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                                    val mapFailed =  Arguments.createMap()
+                                    mapFailed.putString("error",e?.message)
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_FAILED,mapFailed)
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_END, Arguments.createMap())
+                                    return false
+                                }
+
+                                override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                                    val mapSuccess =  Arguments.createMap()
+                                    mapSuccess.putInt("width",resource?.width ?: 0)
+                                    mapSuccess.putInt("height",resource?.height ?: 0)
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_SUCCESS,mapSuccess)
+                                    reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_END, Arguments.createMap())
+                                    return false
+                                }
+                            })
+                }
+                when {
+                    url.contains("base64,") -> {
+                        val s = url.split(",")[1]
+                        val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
+                        manager = manager.load(bytes)
+                    }
+                    url.contains("static;") -> {
+                        val s = url.split("c;")[1]
+                        manager = if(s.contains("http")) {
+                            manager.load(s)
+                        }else{
+                            val id = context.resources.getIdentifier(s,"drawable", context.packageName)
+                            manager.load(id)
+                        }
+                    }
+                    else -> {
+                        manager = manager.load(url)
+                    }
+                }
+
+
+                reactContext.get()?.getJSModule(RCTEventEmitter::class.java)?.receiveEvent(id, EVENT_ON_LOAD_START, Arguments.createMap())
+                manager.apply(options)
                         .into(this)
-            }else Glide.with(context)
-                    .asBitmap()
-                    .load(url)
-                    .apply(options)
-                    .into(this)
-        }
+            }
+
+            Thread.currentThread().interrupt()
+        }.start()
+
 
     }
 
-    private fun updateImageCache(url:String,asGif: Boolean){
-        if (asGif){
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context).asGif().load(bytes).into(this)
-            }else{
-                Glide.with(context).asGif().load(url).into(this)
-            }
-        }else{
-            val options =  RequestOptions()
-                    .frame(0L)
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context)
-                        .asBitmap()
-                        .load(bytes)
-                        .apply(options)
-                        .into(this)
-            }else Glide.with(context)
-                    .asBitmap()
-                    .load(url)
-                    .apply(options)
-                    .into(this)
+
+    private fun getOptions(asGif:Boolean,cache:Boolean,placeholder: String?,resize:Boolean,reqW:Int,reqH:Int):RequestOptions{
+        var options = RequestOptions()
+                .skipMemoryCache(cache)
+
+        load(placeholder)?.toDrawable(context.resources)?.let {
+            options = options.placeholder(it)
         }
+        if(!asGif){
+            options = options.frame(0L)
+        }
+        if(resize){
+            options = options.fitCenter().override(reqW,reqH)
+        }
+
+        return options
     }
 
-    private fun updateImageNoCache(url:String,asGif: Boolean,w:Int,h:Int){
-        if (asGif){
-            val options =  RequestOptions().fitCenter().override(w, h)
-                    .skipMemoryCache(true)
+    private fun load(model: String?) : Bitmap? {
+        if (model.isNullOrEmpty()) return null
 
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context).asGif().load(bytes).apply(options).into(this)
-            }else{
-                Glide.with(context).asGif().load(url).apply(options).into(this)
+        when {
+            model.contains("base64,") -> {
+                val s = model.split(",")[1]
+                val bytes = Base64.decode(s, Base64.DEFAULT)
+                val options =  BitmapFactory.Options()
+                options.inMutable = true
+                return BitmapFactory.decodeByteArray(bytes, 0, bytes.size, options)
             }
-        }else{
-            val options =  RequestOptions()
-                    .frame(0L)
-                    .fitCenter()
-                    .override(w, h)
-                    .skipMemoryCache(true)
-
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context)
-                        .asBitmap()
-                        .load(bytes)
-                        .apply(options)
-                        .into(this)
-            }else Glide.with(context)
-                    .asBitmap()
-                    .load(url)
-                    .apply(options)
-                    .into(this)
-        }
-    }
-    private fun updateImageNoCache(url:String,asGif: Boolean){
-        if (asGif){
-            val options =  RequestOptions()
-                    .skipMemoryCache(true)
-
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context).asGif().load(bytes).apply(options).into(this)
-            }else{
-                Glide.with(context).asGif().load(url).apply(options).into(this)
+            model.contains("static;") -> {
+                val s = model.split("c;")[1]
+                return if(s.contains("http")) {
+                    val url = URL(s)
+                    return try{
+                        BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                    }catch (e: Exception){
+                        null
+                    }
+                }else{
+                    val id = context.resources.getIdentifier(s,"drawable", context.packageName)
+                    ContextCompat.getDrawable(context,id)?.toBitmap()
+                }
             }
-        }else{
-            val options =  RequestOptions()
-                    .frame(0L)
-                    .skipMemoryCache(true)
-            if (url.contains("base64,")){
-                val s = url.split(",")[1]
-                val bytes = android.util.Base64.decode(s,android.util.Base64.DEFAULT)
-                Glide.with(context)
-                        .asBitmap()
-                        .load(bytes)
-                        .apply(options)
-                        .into(this)
-            }else Glide.with(context)
-                    .asBitmap()
-                    .load(url)
-                    .apply(options)
-                    .into(this)
+            else -> {
+                return null
+            }
         }
+
     }
 
 }
