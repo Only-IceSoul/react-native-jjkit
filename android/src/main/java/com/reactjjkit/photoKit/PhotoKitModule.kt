0998c41880ee4d3e3ce73f7febf93834386cb390
Juan <justinjlf21@gmail.com>
2020-10-18 16:13:30 -0400

    pk a

--- android/src/main/java/com/reactjjkit/photoKit/PhotoKitModule.kt
+++ android/src/main/java/com/reactjjkit/photoKit/PhotoKitModule.kt
@@ -18,14 +18,35 @@ import android.provider.MediaStore
 import android.util.Log
 import android.webkit.MimeTypeMap
 import androidx.core.app.ActivityCompat
+import androidx.core.database.getIntOrNull
+import androidx.core.database.getLongOrNull
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.transition.Transition
 import com.facebook.react.bridge.*
 import java.io.ByteArrayOutputStream
 
 class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaModule(context)  {
+
+    companion object{
+        const val RESIZE_MODE_CONTAIN = "contain"
+        const val RESIZE_MODE_COVER = "cover"
+        const val COMPRESS_FORMAT_JPEG = "jpeg"
+        const val COMPRESS_FORMAT_PNG = "png"
+        const val QUERY_IMAGE = "image"
+        const val QUERY_PHOTO = "photo"
+        const val QUERY_GIF = "gif"
+        const val QUERY_VIDEO = "video"
+        const val QUERY_VIDEO_PHOTO = "video_photo"
+        const val QUERY_VIDEO_GIF = "video_gif"
+        const val QUERY_ALL = "all"
+
+    }
 
    private val reactContext: ReactApplicationContext = context
     private val mListener = object : ActivityEventListener {
@@ -62,98 +83,111 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                 "gif" to "gif",
                 "photo" to "photo",
                 "all" to "all",
-                "jpeg" to 0,
-                "png" to 1,
-                 "AUTHORIZED" to 1,
+                "AUTHORIZED" to 1,
                 "UNDETERMINED" to 0,
-                "DENIED" to 2,
-                "cover" to 1,
-                "contain" to 0,
-                "AUTOMATIC" to 0,
-                "NONE" to 1,
-                "ALL" to 2,
-                "DATA" to 3,
-                "RESOURCE" to 4)
+                "DENIED" to 2
+              )
     }
 
   @ReactMethod
     fun clearMemoryCache(promise:Promise){
         Handler(Looper.getMainLooper()).post {
-            Glide.get(reactContext).clearMemory()
-            promise.resolve(true)
-          
+            try {
+                Glide.get(reactContext).clearMemory()
+                promise.resolve(true)
+            }catch (e:Exception){
+                promise.reject(e)
+            }
+
         }
     }
 
     @ReactMethod
     fun isPermissionGranted(promise: Promise){
-        val bool =  ActivityCompat.checkSelfPermission(reactContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                &&  ActivityCompat.checkSelfPermission(reactContext, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-        promise.resolve(bool)
+        try {
+            val bool =  ActivityCompat.checkSelfPermission(reactContext, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+                    &&  ActivityCompat.checkSelfPermission(reactContext, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+            promise.resolve(bool)
+        }catch (e:Exception){
+            promise.reject(e)
+        }
+
     }
 
 
     private var mPromise : Promise? = null
     @ReactMethod
     fun requestPermission(promise: Promise){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            val i = Intent(reactContext, PermissionActivity::class.java)
-            reactContext.addActivityEventListener(mListener)
-            reactContext.startActivityForResult(i,200, Bundle())
-            mPromise = promise
-        }else{
-            promise.resolve(1)
+        try{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val i = Intent(reactContext, PermissionActivity::class.java)
+                reactContext.addActivityEventListener(mListener)
+                reactContext.startActivityForResult(i,200, Bundle())
+                mPromise = promise
+            }else{
+                promise.resolve(1)
+            }
+        }catch (e:Exception){
+            promise.reject(e)
         }
+
     }
 
 
     @ReactMethod
     fun requestRaw(data:String?,promise: Promise){
-       val bytes = reactContext.contentResolver.openInputStream(Uri.parse(data))?.readBytes()
-        if (bytes != null && bytes.isNotEmpty()){
+        try{
+            val bytes = reactContext.contentResolver.openInputStream(Uri.parse(data))?.readBytes()
             val str = android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)
             promise.resolve(str)
-        }else{
-            promise.resolve(null)
+        }catch (e:Exception){
+            promise.reject(e)
         }
     }
 
     @ReactMethod
     fun requestImage(data:ReadableMap?,promise: Promise){
-        val identifier = try { data!!.getString("uri") } catch (e:Exception){ null}
+        val uri = try { data!!.getString("uri") } catch (e:Exception){ null}
         val width = try { data!!.getInt("width") } catch (e:Exception){  600 }
         val height =  try { data!!.getInt("height") } catch (e:Exception){  600 }
+        val resizeMode =  try { data!!.getString("resizeMode") } catch (e:Exception) { RESIZE_MODE_CONTAIN }
         val quality =  try { data!!.getDouble("quality") } catch (e:Exception){ 1.0}
-        val format =  try { data!!.getInt("format") } catch (e:Exception){ 0 }
+        val format =  try { data!!.getString("format") } catch (e:Exception){ COMPRESS_FORMAT_JPEG }
 
-        if(identifier!= null && identifier.isNotEmpty()){
-            val options = RequestOptions().fitCenter().frame(0L).override(width,height)
 
-            Glide.with(reactContext).asBitmap().load(identifier)
-                    .apply(options)
-                    .into(object: CustomTarget<Bitmap>(){
-                override fun onLoadCleared(placeholder: Drawable?) {}
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    val baos = ByteArrayOutputStream()
-                    val q = (100 * quality).toInt()
-                    if (format == 0){
-                        if(resource.compress(Bitmap.CompressFormat.JPEG, q, baos)){
-                            val bytes = baos.toByteArray()
-                            val str = android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)
-                            promise.resolve(str)
-                        }else promise.resolve(null)
-                    }else{
-                        if(resource.compress(Bitmap.CompressFormat.PNG, q, baos)){
-                            val bytes = baos.toByteArray()
-                            val str = android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)
-                            promise.resolve(str)
-                        }else promise.resolve(null)
+        val options = if(resizeMode == RESIZE_MODE_COVER)  RequestOptions().centerCrop().frame(0L).override(width,height)
+        else  RequestOptions().fitCenter().frame(0L).override(width,height)
+
+        Glide.with(reactContext).asBitmap().load(uri)
+                .apply(options)
+                .listener(object :RequestListener<Bitmap> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                        promise.reject(e)
+                        return true
                     }
-                }
-            })
-        }else{
-            promise.resolve(null)
-        }
+                    override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                       try {
+                           val bytesStream = ByteArrayOutputStream()
+                           val q = (100 * quality).toInt()
+                           if (format == COMPRESS_FORMAT_PNG){
+                               resource!!.compress(Bitmap.CompressFormat.PNG, q, bytesStream)
+                           }else{
+                               resource!!.compress(Bitmap.CompressFormat.JPEG, q, bytesStream)
+                           }
+                           val bytes = bytesStream.toByteArray()
+                           val str = android.util.Base64.encodeToString(bytes, android.util.Base64.DEFAULT)
+                           promise.resolve(str)
+                       }catch (e:Exception){
+                           promise.reject(e)
+                       }
+                        return true
+                    }
+                })
+                .into(object: CustomTarget<Bitmap>(){
+                    override fun onLoadCleared(placeholder: Drawable?) {}
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {}
+                })
+
     }
 
 
@@ -162,7 +196,7 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
     fun fetch(query:ReadableMap?, promise: Promise)  {
         Thread {
             try{
-                val media = try {  query!!.getString("query") }catch(e: java.lang.Exception) {  "all" }
+                val media = try {  query!!.getString("query")!! }catch(e: java.lang.Exception) {  QUERY_ALL }
                 val names =  try {  query!!.getArray("names") }catch(e: java.lang.Exception) {  null }
                 val limit = try {   query!!.getInt("limit") }catch(e: java.lang.Exception) {  -1 }
                 val offset = try {   query!!.getInt("offset") }catch(e: java.lang.Exception) {  -1 }
@@ -196,36 +230,37 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                 }
 
                 val wFilterImage = if(where.isNotEmpty()) "($where) AND ${MediaStore.Images.Media.MIME_TYPE} != ?"  else "${MediaStore.Images.Media.MIME_TYPE} != ?"
+                val wFilterGif = if(where.isNotEmpty()) "($where) AND ${MediaStore.Images.Media.MIME_TYPE} == ?"  else "${MediaStore.Images.Media.MIME_TYPE} == ?"
                 val wVideo = if(where.isNotEmpty()) where else null
                 val argsVideo = if(whereArgs.isNotEmpty()) whereArgs.toTypedArray() else null
                 val wImage = if(where.isNotEmpty()) where else null
                 val argsImage = if(whereArgs.isNotEmpty()) whereArgs.toTypedArray() else null
 
                 when(media){
-                    "image" -> {
+                    QUERY_IMAGE -> {
 
                         getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wImage, argsImage,l,off)
                     }
-                    "video" -> {
+                    QUERY_VIDEO -> {
                         getGalleryVideos(reactContext,galleryAlbums,galleryMedia,albumsNames,wVideo,argsVideo,l,off)
                     }
-                    "photo" ->{
+                    QUERY_PHOTO ->{
                         val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("gif") ?: "image/gif"
                         whereArgs.add(mimeType)
                         getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterImage,whereArgs.toTypedArray(),l,off)
                     }
-                    "gif"->{
+                    QUERY_GIF ->{
                         val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("gif") ?: "image/gif"
                         whereArgs.add(mimeType)
-                        getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterImage,whereArgs.toTypedArray(),l,off)
+                        getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterGif,whereArgs.toTypedArray(),l,off)
                     }
-                    "video_gif"->{
+                    QUERY_VIDEO_GIF->{
                         val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("gif") ?: "image/gif"
                         whereArgs.add(mimeType)
-                        getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterImage,whereArgs.toTypedArray(),l,off)
+                        getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterGif,whereArgs.toTypedArray(),l,off)
                         getGalleryVideos(reactContext,galleryAlbums,galleryMedia,albumsNames,wVideo,argsVideo,l,off)
                     }
-                    "video_photo" ->{
+                    QUERY_VIDEO_PHOTO ->{
                         val mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension("gif") ?: "image/gif"
                         whereArgs.add(mimeType)
                         getGalleryImages(reactContext,galleryAlbums,galleryMedia,albumsNames,wFilterImage,whereArgs.toTypedArray(),l,off)
@@ -412,7 +447,7 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                         galleryAlbums.add(album)
                         currentAlbums.add(it)
                     }else{
-                        galleryAlbums.forEachIndexed { index, item ->
+                        galleryAlbums.forEachIndexed { _, item ->
                             if(item["name"] as String == it){
                                 item["count"] = item["count"] as Int + cursor.count
                             }
@@ -441,7 +476,10 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                     MediaStore.Video.Media._ID,
                     MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
                     MediaStore.Video.Media.DATE_ADDED,
-                    MediaStore.Video.Media.DISPLAY_NAME
+                    MediaStore.Video.Media.DISPLAY_NAME,
+                    MediaStore.Video.Media.WIDTH,
+                    MediaStore.Video.Media.HEIGHT,
+                    MediaStore.Video.Media.DURATION
             )
 
         val orderBy = "${MediaStore.Images.Media.DATE_ADDED} DESC"
@@ -460,6 +498,9 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                         videoCursor.getColumnIndex(MediaStore.Video.Media.DATE_ADDED)
                 val displayName =
                         videoCursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME)
+                val wc = videoCursor.getColumnIndex( MediaStore.Video.Media.WIDTH)
+                val hc = videoCursor.getColumnIndex( MediaStore.Video.Media.HEIGHT)
+                val dc = videoCursor.getColumnIndex(MediaStore.Video.Media.DURATION)
 
                 do {
                     val id = videoCursor.getString(idColumn)
@@ -469,14 +510,17 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
 
                     val media = mutableMapOf<String,Any>()
                     val uri = Uri.withAppendedPath(videoQueryUri, id)
-                    val metadata = getVideoMetadata(ctx,uri)
+
+                    val d = videoCursor.getLongOrNull(dc) ?: 0L
+                    val w = videoCursor.getIntOrNull(wc) ?: 0
+                    val h = videoCursor.getIntOrNull(hc) ?: 0
 
                     media["albumName"] = data
                     media["displayName"] = nameWithFormat
                     media["date"] = dateAdded.toLong()
-                    media["duration"] = metadata[2]
-                    media["height"] = metadata[1]
-                    media["width"] = metadata[0]
+                    media["duration"] = d.toDouble() / 1000
+                    media["height"] = h
+                    media["width"] = w
                     media["mediaType"] = "video"
                     media["uri"] = uri.toString()
 
@@ -522,7 +566,9 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                 MediaStore.Images.Media.BUCKET_DISPLAY_NAME,
                 MediaStore.Images.Media.DATE_ADDED,
                 MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.MIME_TYPE
+                MediaStore.Images.Media.MIME_TYPE,
+                MediaStore.Images.Media.WIDTH,
+                MediaStore.Images.Media.HEIGHT
         )
 
         val orderBy = "${MediaStore.Images.Media.DATE_ADDED} DESC"
@@ -543,23 +589,26 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
                         imagesCursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME)
 
                 val mimeType = imagesCursor.getColumnIndex( MediaStore.Images.Media.MIME_TYPE)
-
+                val wc = imagesCursor.getColumnIndex( MediaStore.Images.Media.WIDTH)
+                val hc = imagesCursor.getColumnIndex( MediaStore.Images.Media.HEIGHT)
                 do {
                     val id = imagesCursor.getString(idColumn)
                     val data = imagesCursor.getString(dataColumn)
                     val dateAdded = imagesCursor.getString(dateAddedColumn)
                     val nameWithFormat = imagesCursor.getString(displayName)
                     val type = imagesCursor.getString(mimeType)
+                    val w = imagesCursor.getIntOrNull(wc) ?: 0
+                    val h = imagesCursor.getIntOrNull(hc) ?: 0
 
                     val media = mutableMapOf<String,Any>()
                     val uri  = Uri.withAppendedPath(imagesQueryUri, id)
-                    val metadata = getPhotoMetadata(ctx,uri)
+
                     media["albumName"] = data
                     media["displayName"] = nameWithFormat
                     media["date"] = dateAdded.toLong()
                     media["duration"] = 0
-                    media["height"] = metadata[1]
-                    media["width"] = metadata[0]
+                    media["height"] = w
+                    media["width"] = h
                     media["mediaType"] = if(type.contains("image/gif")) "gif" else "image"
                     media["uri"] = uri.toString()
 
@@ -608,13 +657,13 @@ class PhotoKitModule(context: ReactApplicationContext) : ReactContextBaseJavaMod
         return try{
             val retriever =  MediaMetadataRetriever()
             retriever.setDataSource(ctx, uri)
-            val height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT) ?: "0"
-            val width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH) ?: "0"
+//            val height = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT) ?: "0"
+//            val width = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH) ?: "0"
             val time =  retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION) ?: "0"
             retriever.release()
             val duration = time.toDouble() / 1000
             //milliSeg  1000 = 1 seg
-            doubleArrayOf(width.toDouble(),height.toDouble(),duration)
+            doubleArrayOf(0.0,0.0,duration)
         }catch (e:Exception){
             doubleArrayOf(0.0,0.0,0.0)
         }
